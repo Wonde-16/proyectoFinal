859aab4814a7c9cecf6d0e1d1a96f4911485d917
Wonde-16 <115214551+Wonde-16@users.noreply.github.com>
2023-05-24 23:58:36 +0200

    24/05/2023

---
+++ js/estilo.js
@@ -0,0 +1,23 @@
+import { CountUp } from '../countUp.js/dist/countUp.js';
+
+var cont = 0
+
+window.addEventListener("scroll",function(){
+    const  sectionPrimera  =document.querySelector(".primero");
+    let al1 = sectionPrimera.getBoundingClientRect();
+
+    if(al1.top <= "0" && cont == 0){
+        var countUp = new CountUp('contador1', 32000);
+        countUp.start();
+        var countUp = new CountUp('contador2', 7000);
+        countUp.start();
+        var countUp = new CountUp('contador3', 1400000);
+        countUp.start();
+        var countUp = new CountUp('contador4', 300);
+        countUp.start();
+        cont = cont+1
+    }
+  
+})
+
+
